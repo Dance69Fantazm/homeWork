9293fea228146180003c715d963a6886e345a409
GANS <angel-ise-demon@yandex.ru>
2020-09-30 19:42:07 +0300

    Full home project №3

---
+++ src/main/java/geekbrains/homeWork3/GuessNumber.java
@@ -0,0 +1,50 @@
+package geekbrains.homeWork3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        numberRandoms();
+    }
+
+    public static void numberRandoms() {
+        int score = 3;
+        int maxCount = 3;
+        Random random = new Random();
+        int numberRand = random.nextInt(10);
+        System.out.println("Угадайте число от 0 до 9, у вас " + maxCount + " попытки!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+
+        for (int tryCount = 1; tryCount <= maxCount; tryCount++) {
+            int youNumber = scanner.nextInt();
+
+            if (youNumber == numberRand) {
+                System.out.println("Вы угадали!");
+                break;
+            } else if (youNumber > numberRand){
+                score--;
+                System.out.println("Вы не угадали!" + "\n У вас осталась " + score + " попыток" + "\n  Вы ввели слишком большое число");
+            } else {
+                score--;
+                System.out.println("Вы не угадали!" + "\n У вас осталась " + score + " попыток" + "\n  Вы ввели слишком маленькое число");
+            }
+            if (tryCount == maxCount)
+                System.out.println("Вы проиграли!");
+        }
+
+        System.out.println("Повторить игру еще раз? 1 - да / 0 - нет");
+        int yesNo = scanner.nextInt();
+        while (true){
+            if (yesNo == 1){
+                numberRandoms();
+                break;
+            } else if (yesNo == 0){
+                System.out.println("Конец игры!");
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/geekbrains/homeWork3/Puzzle.java
@@ -0,0 +1,37 @@
+package geekbrains.homeWork3;
+
+import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Puzzle {
+    /*public static void main(String[] args) {
+        issueName();
+    }
+    public static void issueName(){
+        String[] words = new String[]
+                {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+                        "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
+                        "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+                        "pepper", "pineapple", "pumpkin", "potato"};
+        Random random = new Random();
+        int nameRandomWords = random.nextInt(words.length);
+        System.out.println("Угадайте загаданное слово");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Напишите ваше слово: ");
+
+        for (int i = 0; i < 10; i++) {
+            int myWord = scanner.nextInt();
+
+            if (myWord.equals(nameRandomWords)) {
+                System.out.println("Вы угадали слово! Поздравляю! " + "\nПравильное слово: " + nameRandomWords);
+                break;
+            } else {
+                System.out.println("Попробуйте снова! У вас получится! " + nameRandomWords);
+            }
+        }
+        scanner.close();
+
+    }*/
+}
